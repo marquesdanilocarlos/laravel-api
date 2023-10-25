a78f8fff5714aeede4656a4f423157f7e5fe139c
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-10-25 16:38:56 -0300

    Aula 02 - Rotas api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/alunos', function(){
+    echo 'Lista de alunos';
+});
+
+Route::get('/alunos/{id}', function(){
+    echo 'Dados do aluno';
+});
+
+Route::post('/alunos', function(){
+    echo 'Inclusão de alunos';
+});
+
+Route::put('/alunos/1', function (){
+    echo 'Edição de aluno';
+});
+
+Route::delete('/alunos/1', function (){
+    echo 'Deleção de aluno';
+});
