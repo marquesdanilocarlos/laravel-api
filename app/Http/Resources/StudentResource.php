34ef46a6b475d5ba29c09f59158a482881493e5a
Danilo Carlos Marques <danilo.marques@basis.com.br>
2023-10-27 15:21:22 -0300

    Aula 18 - Adição de links HATEOAS no Resource

--- app/Http/Resources/StudentResource.php
+++ app/Http/Resources/StudentResource.php
@@ -20,8 +20,13 @@ class StudentResource extends JsonResource
             'birth_date' => $this->birth_date->format('d/m/Y'),
             'gender' => $this->gender,
             'class_id' => $this->course_class_id,
-            'class' => new CourseClassResourse($this->courseClass)
+            'class' => new CourseClassResourse($this->courseClass),
             //'class' => new CourseClassResourse($this->whenLoaded('courseClass'));
+            'links' => [
+                ['type' => 'GET', 'url' => route('students.show', $this->id), 'rel' => 'student_details'],
+                ['type' => 'PUT', 'url' => route('students.update', $this->id), 'rel' => 'student_update'],
+                ['type' => 'DELETE', 'url' => route('students.destroy', $this->id), 'rel' => 'student_delete']
+            ]
         ];
     }
 }
